5f3cc4ac3c2cbc3ef21cfded4e885b0de64b1ba0
leanderfr <leanderprogrammer@gmail.com>
2025-11-15 15:09:07 -0300

    Atl

---
+++ backend_nodejs/index.js
@@ -0,0 +1,31 @@
+
+
+//const tmp = require('./teste')
+
+//const os = require('os')
+
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method)
+//  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Content-Type', 'text/html')
+//  res.write("<div style='font-size:40px;color:red'>teste legal</div>")
+
+  fs.readFile('./teste.html', (err, data) => {
+    if (err) {
+      console.log("erro= ", err) 
+    } else {
+      //res.write(data)
+      res.statusCode=400
+      res.end(data);
+    }
+  })
+  
+})
+
+server.listen(3000, 'localhost') 
+
+//console.log(tmp.people)
+//console.log(os.platform() , os.homedir())
--- backend_nodejs/teste.js
+++ backend_nodejs/teste.js
@@ -1,4 +1,11 @@
 
 
-console.log(__dirname)
-console.log(__filename)
+//console.log(__dirname)
+//console.log(__filename)
+
+const people = [ "Fabian", "Tamara"]
+const ages = [ 49, 32 ]
+
+module.exports = {
+  people, ages
+}
